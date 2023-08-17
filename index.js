bc9a923285a54e2222f694b4e394e799ddd78e6a
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-08-17 15:17:13 +0600

    node server req res example

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ const http = require('http')
 
 // create server
 const server = http.createServer((req, res)=>{
-    res.end('Hello World')
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h1>Home Page</h1>')
+        res.end()
+    }
+    else if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    else if (req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h1>Contact Page</h1>')
+        res.end()
+    }
+
+    // can add content type for json
+    else if (req.url === '/api'){
+        const users = [
+            {name: 'Bob Smith', age: 40},
+            {name: 'John Doe', age: 30}
+        ]
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.write(JSON.stringify(users))
+        res.end()
+    }
 })
 
 // define port
